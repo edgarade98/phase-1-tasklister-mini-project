586b5712599dcdf0a64563c4e41b1227c6815bbc
Edgar <eddyade98@gmail.com>
2023-10-25 13:29:28 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  // your code here 
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+     e.preventDefault()
+     toDo(e.target.new_task_description.value)
+     form.reset
+  })
+})
+
+function toDo (todo) {
+  let p = document.createElement ('p')
+  let btn = document.createElement ('button')
+  btn.addEventListener ('click', takeDelete)
+  btn.textContent = 'x'
+  p.textContent = todo
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function takeDelete (e){
+    e.target.parentNode.remove()
+}
